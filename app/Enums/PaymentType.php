9731d925f0fff0e3ad7420c0fab55a6b9455af74
Pangski <ethanpanggabean@gmail.com>
2025-07-28 18:48:23 +0700

    Update PaymentType.php

--- app/Enums/PaymentType.php
+++ app/Enums/PaymentType.php
@@ -9,13 +9,21 @@ enum PaymentType: string
     case CREDIT = 'Kartu Kredit';
     case EWALLET = 'Dompet Elektronik';
 
+    /**
+     * Menghasilkan daftar opsi untuk dropdown atau pilihan radio.
+     *
+     * @param array $exclude Nama enum yang ingin dikecualikan
+     * @return array Array dengan value dan label
+     */
     public static function options(array $exclude = []): array
     {
         return collect(self::cases())
-            ->filter(fn ($item) => ! in_array($item->name, $exclude))
+            ->filter(fn ($item) => !in_array($item->name, $exclude))
             ->map(fn ($item) => [
                 'value' => $item->value,
                 'label' => $item->value,
-            ])->values()->toArray();
+            ])
+            ->values()
+            ->toArray();
     }
 }
