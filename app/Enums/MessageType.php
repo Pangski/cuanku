0e740a1546a62ad5a54313c279bf23894a30203e
Pangski <ethanpanggabean@gmail.com>
2025-07-28 18:39:07 +0700

    Update MessageType.php

--- app/Enums/MessageType.php
+++ app/Enums/MessageType.php
@@ -7,14 +7,25 @@ enum MessageType: string
     case CREATED = 'Berhasil menambahkan';
     case UPDATED = 'Berhasil memperbarui';
     case DELETED = 'Berhasil menghapus';
-    case ERROR = 'Terjadi kesalahan. Silakan coba lagi nanti';
+    case ERROR   = 'Terjadi kesalahan. Silakan coba lagi nanti';
 
+    /**
+     * Menghasilkan pesan status berdasarkan tipe message
+     *
+     * @param string $entity Nama entitas (opsional, contoh: "Transaksi")
+     * @param string|null $error Pesan error jika tipe adalah ERROR
+     * @return string
+     */
     public function message(string $entity = '', ?string $error = null): string
     {
-        if ($this === MessageType::ERROR && $error) {
-            return "{$this->value}. Error: {$error}";
+        $message = $this->value;
+
+        if ($this === self::ERROR && $error) {
+            return "{$message} Error: {$error}";
         }
 
-        return "{$this->value} {$entity}.";
+        return $entity
+            ? "{$message} {$entity}."
+            : "{$message}.";
     }
 }
