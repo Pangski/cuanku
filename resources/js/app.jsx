97fcd97e851d6df52de84a1fe7803849e5de0bd5
Pangski <ethanpanggabean@gmail.com>
2025-07-28 19:35:41 +0700

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,26 +7,28 @@ import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from './Components/Dark/ThemeProvider';
 
+// Nama default aplikasi
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+// Inisialisasi aplikasi Inertia
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+
     setup({ el, App, props }) {
-        const appElement = (
+        const root = createRoot(el);
+
+        // Membungkus komponen utama dengan ThemeProvider dan Toaster
+        root.render(
             <ThemeProvider defaultTheme="light" storageKey="current-theme">
                 <App {...props} />
                 <Toaster position="top-center" richColors />
             </ThemeProvider>
         );
-
-        createRoot(el).render(appElement);
-
-        // const root = createRoot(el);
-
-        // root.render(<App {...props} />);
     },
+
     progress: {
-        color: '#4B5563',
+        color: '#4B5563', // Tailwind gray-600
     },
 });
