09c4ca9266721b9c9dfdf7c01c315073bc250d32
Pangski <ethanpanggabean@gmail.com>
2025-07-29 08:53:13 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Command untuk menampilkan kutipan inspiratif
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
